b5767a32caf1451c51916bd096367d08655de36c
AndyBogy <andrew.bogush@wsu.edu>
2025-03-31 14:11:36 -0700

    fatso

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+#C++ code
+<<<<<<< HEAD
+butt chheks
+
+
+=======
+your mommas big booty cheeks
+>>>>>>> 27915cbfef3ac4fdf7259dc4c371523debf581ba
+
+Josh is a big poo poo  kaa kaa
+
+=======
 # include <iostream>
 using namespace std;
 
@@ -9,4 +22,7 @@ int main{
     cout<< "penis length: "<< length <<endl;
     cout<< "your mommas big booty cheeks"<<endl;
 
+    
 }
+
+>>>>>>> 52c728b549df915b808a83a0accefceaeea0625b
